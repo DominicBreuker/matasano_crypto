fad71243a669dbd49c3e393b5ac204ff89ad479f
Dominic Breuker <dominic.breuker@gmail.com>
2017-08-19 11:24:56 +0200

    add method to print hex strings of byte sequences

--- set1/c5.py
+++ set1/c5.py
@@ -10,9 +10,13 @@ def repeating_key_xor(b, key):
         result.append(char ^ key[i % key_length])
     return bytes(result)
 
+def hex_string(b):
+    return binascii.hexlify(b).decode("utf-8")
 
 if __name__ == "__main__":
     plaintext = str.encode("Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal")
     key = str.encode("ICE")
     ciphertext = bytes.fromhex("0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f")
-    assert repeating_key_xor(plaintext, key) == ciphertext, "ERROR!!!"
+    decryption = repeating_key_xor(plaintext, key)
+    print("Decrypted bytes (hex): {}".format(hex_string(decryption)))
+    assert decryption == ciphertext, "ERROR!!!"
